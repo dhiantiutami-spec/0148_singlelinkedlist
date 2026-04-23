1569012dec28c68e569e015411e638e1672279e5
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-04-23 11:30:06 +0700

    menambah looping

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -39,5 +39,13 @@ public:
             START = nodeBaru;
             return;
         }
+        Node *previous = START;
+        Node *current = START;
+
+        while (current != NULL && nim > current->noMhs)
+        {
+            
     }
+
+    
 };
