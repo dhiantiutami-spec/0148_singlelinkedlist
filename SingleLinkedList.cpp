6468b44f5657d5e28a5ed444672c7b4c07def41e
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-04-23 11:06:51 +0700

    menambah class LinkedList

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -5,5 +5,16 @@ using namespace std;
 class Node
 {
 public:
-  
+    int noMhs;
+    Node *next;
 };
+
+class LinkedList
+{
+    Node *Start;
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+}
