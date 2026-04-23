8b60b5c170d4820a5b1baec3987a10107ca77786
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-04-23 11:30:39 +0700

    menambah conditional statement

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -44,7 +44,16 @@ public:
 
         while (current != NULL && nim > current->noMhs)
         {
-            
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
     }
 
     
